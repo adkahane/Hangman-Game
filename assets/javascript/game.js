a17c9631c7a507a07b68a3ddd485cfab92888db5
adkahane <adam.kahane.nj@gmail.com>
2018-01-18 12:50:49 -0800

    more functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,47 +3,71 @@
 
 var wordList = ["BURZUM", "MAYHEM", "DEATH", "GORGOROTH", "NORWAY", "BATHORY", "GOATS"];
 var lettersUsed = [];
+var dashWord = [];
+var playWord;
 var wins = 0;
 var guessesLeft = 8;
 
 // Functions
 
-function compareUsed(used, guessed) {
-
+function randomWord(wordList) {
+  return(wordList[Math.floor(Math.random()*wordList.length())]);
 }
 
-function compareWord(word, guessed) {
-
-}
-
-function newGame(play) {
-  theGame();
-}
-// Main
-// Every time the user presses a key this will run
-function theGame() {
-  document.onkeyup = function(event) {
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    console.log(userGuess);
-    // Game Logic      
-    if (userGuess === compareUsed(userGuess)) {
-      // Shows the game progression and stats
-      var html =
-        "<p>You chose: " + userGuess + "</p>" +
-        "<p>The computer chose: " + computerGuess + "</p>" +
-        "<p>wins: " + wins + "</p>" +
-        "<p>losses: " + losses + "</p>" +
-        "<p>ties: " + ties + "</p>";
-
-    // Set the inner HTML contents of the #game div to our html string
-    document.querySelector("#game").innerHTML = html;
+// Checks if the user has guessed the letter before
+function compareUsed(used, guess) {
+  for(i = 1; i < guessed.length(); i++) {
+    if (guess === used[i]) {
+      return(true);
     }
-
     else {
-      document.querySelector("#game").innerHTML = "<p> Please only press R, P or S. Thank you!</p>"
+      return(false);
     }
   }
 }
 
-theGame();
+// Checks if the user's guess is in the current word
+function compareWord(word, guess) {
+  for(i = 1; i < word.length(); i++) {
+    if (guess === word[i]) {
+      return(true);
+    }
+    else {
+      return(false);
+    }
+  }
+}
+
+// Main
+// Every time the user presses a key this will run
+function theGame() {
+  for(guessesLeft = 8; guessesLeft > 0; guessesLeft--) {
+    document.onkeyup = function(event) {
+      // Determines which key was pressed.
+      var userGuess = event.key;
+      console.log(userGuess);
+      // Game Logic
+      if (compareUsed(lettersUsed, userGuess) === true) {
+        // Shows the game progression and stats
+        var html =
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Word: " + computerGuess + "</p>" +
+          "<p>wins: " + wins + "</p>" +
+          "<p>losses: " + losses + "</p>" +
+          "<p>ties: " + ties + "</p>";
+
+      // Set the inner HTML contents of the #game div to our html string
+      document.querySelector("#game").innerHTML = html;
+      }
+
+      else {
+        document.querySelector("#man").innerHTML = "<p> Please only press R, P or S. Thank you!</p>"
+      }
+    }
+  }
+}
+
+while(true) {
+  randomWord(wordList);
+  theGame();
+}
